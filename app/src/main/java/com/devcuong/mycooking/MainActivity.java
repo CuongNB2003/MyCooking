a0702e629ff2b62ff73c1d774f527314b83f439e
cuongnbph22662 <bacuong15423@gmail.com>
2024-02-22 12:55:45 +0700

    bottom_navigation_commit

--- app/src/main/java/com/devcuong/mycooking/MainActivity.java
+++ app/src/main/java/com/devcuong/mycooking/MainActivity.java
@@ -1,14 +1,44 @@
 package com.devcuong.mycooking;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.devcuong.mycooking.R;
 import android.os.Bundle;
+import com.devcuong.mycooking.databinding.ActivityMainBinding;
+import com.devcuong.mycooking.fragments.CategoryFragment;
+import com.devcuong.mycooking.fragments.FavoriteFragment;
+import com.devcuong.mycooking.fragments.HomeFragment;
+import com.devcuong.mycooking.fragments.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragmet(new HomeFragment());
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home_nav) {
+                replaceFragmet(new HomeFragment());
+            } else if (id == R.id.favorite_nav) {
+                replaceFragmet(new FavoriteFragment());
+            } else if (id == R.id.category_nav) {
+                replaceFragmet(new CategoryFragment());
+            } else if (id == R.id.setting_nav) {
+                replaceFragmet(new SettingFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragmet(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/devcuong/mycooking/fragments/SettingFragment.java
@@ -0,0 +1,33 @@
+package com.devcuong.mycooking.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.devcuong.mycooking.R;
+
+public class SettingFragment extends Fragment {
+    public SettingFragment() {
+        // Required empty public constructor
+    }
+    public static SettingFragment newInstance(String param1, String param2) {
+        SettingFragment fragment = new SettingFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+}
