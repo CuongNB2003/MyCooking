a7d5587c159e5274323874f4c78af1bdc656b1fc
cuongnbph22662 <bacuong15423@gmail.com>
2024-02-24 21:20:44 +0700

    meal_category_done

---
+++ app/src/main/java/com/devcuong/mycooking/obj/ListMeal.java
@@ -0,0 +1,15 @@
+package com.devcuong.mycooking.obj;
+
+import java.util.List;
+
+public class ListMeal {
+    private List<Meal> meals;
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+}
---
+++ app/src/main/java/com/devcuong/mycooking/obj/Meal.java
@@ -0,0 +1,85 @@
+package com.devcuong.mycooking.obj;
+
+public class Meal {
+    private String idMeal; // id
+    private String strMeal; // ten
+    private String strCategory; // the loai
+    private String strArea; // nguon goc
+    private String strInstructions; // cach nau
+    private String strMealThumb; // hinh anh
+    private String strYoutube; // link youtobe
+    private String strSource; // link web co the co hoac khong
+    private String strTags; // nguyen lieu
+
+    public String getStrTags() {
+        return strTags;
+    }
+
+    public void setStrTags(String strTags) {
+        this.strTags = strTags;
+    }
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public void setStrCategory(String strCategory) {
+        this.strCategory = strCategory;
+    }
+
+    public String getStrArea() {
+        return strArea;
+    }
+
+    public void setStrArea(String strArea) {
+        this.strArea = strArea;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public void setStrInstructions(String strInstructions) {
+        this.strInstructions = strInstructions;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+
+    public String getStrYoutube() {
+        return strYoutube;
+    }
+
+    public void setStrYoutube(String strYoutube) {
+        this.strYoutube = strYoutube;
+    }
+
+    public String getStrSource() {
+        return strSource;
+    }
+
+    public void setStrSource(String strSource) {
+        this.strSource = strSource;
+    }
+}
